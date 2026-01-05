33710ed669a0ce7c881a9d2f3b445e92ecb3edde
wisemantattoo <wisemantattoo@gmail.com>
2026-01-05 11:46:24 +0200

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -3,9 +3,13 @@ import os
 SECRETS = {
     "GEMINI_KEY": os.environ.get("GEMINI_API_KEY"),
     "UNSPLASH_KEY": os.environ.get("UNSPLASH_ACCESS_KEY"),
-    "CLIENT_SECRET_RAW": os.environ.get("CLIENT_SECRET_JSON"),
+    # המפתחות החדשים והבטוחים של גוגל [cite: 2026-01-05]
+    "CLIENT_ID": os.environ.get("GOOGLE_CLIENT_ID"),
+    "CLIENT_SECRET": os.environ.get("GOOGLE_CLIENT_SECRET"),
     "REFRESH_TOKEN": os.environ.get("YOUTUBE_REFRESH_TOKEN"),
-    "TIKTOK_TOKEN": os.environ.get("TIKTOK_ACCESS_TOKEN")
+    # שמירה על נתוני הטיקטוק שלך [cite: 2025-12-26]
+    "TIKTOK_KEY": os.environ.get("TIKTOK_CLIENT_KEY"),
+    "TIKTOK_SECRET": os.environ.get("TIKTOK_CLIENT_SECRET")
 }
 
 GUMROAD_LINK = "https://thebrainlabofficial.gumroad.com/l/vioono"
